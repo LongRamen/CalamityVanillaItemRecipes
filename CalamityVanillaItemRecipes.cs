04da7680aaaba51ac05ab3dfa214032c142233e9
LongRamen <106673028+LongRamen@users.noreply.github.com>
2026-07-28 00:22:54 +0100

    Remove out of date records

--- CalamityVanillaItemRecipes.cs
+++ CalamityVanillaItemRecipes.cs
@@ -3,9 +3,3 @@ using Terraria.ModLoader;
 namespace CalamityVanillaItemRecipes;
 
 public class CalamityVanillaItemRecipes : Mod;
-
-// new items: blood orb, essence of eleum, ancient bone dust, murky paste, core of sunlight, essence of sunlight, demonic bone ash
-// new groups: evil powder, copper bar, cobalt bar, gold bar, ice block, stone block
-
-// unsuitable items: pearl shard, rogue emblem, astral clay, starblight soot, sea prism, sea minnow, piggy, aerialite bar
-// unsuitable groups: any snow block
