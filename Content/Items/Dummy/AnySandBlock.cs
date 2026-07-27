9574ed46299f156d79e9ec9d93065f9412175d2e
LongRamen <106673028+LongRamen@users.noreply.github.com>
2026-07-28 00:30:28 +0100

    Add AnySandBlock dummy item

---
+++ Content/Items/Dummy/AnySandBlock.cs
@@ -0,0 +1,9 @@
+﻿namespace CalamityVanillaItemRecipes.Content.Items.Dummy;
+
+public class AnySandBlock : RecipeGroupDummyItem
+{
+    protected override int FrameCount => 8;
+
+    protected override int Width => 12;
+    protected override int Height => 12;
+}
